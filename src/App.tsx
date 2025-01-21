b1b709a115ae14a92688be906550cf20b68aae2e
ladianchad <qhrejddlvltm@gmail.com>
2025-01-21 16:33:29 +0900

    add analistic
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import Home from "./pages/home/Home";
+import {BrowserRouter} from "react-router-dom";
+import {Route, Routes} from "react-router";
+
 const App = () => {
     return <BrowserRouter>
         <Routes>
